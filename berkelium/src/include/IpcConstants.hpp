0dc5291a4787070910bef8e76c63d787b1ab4721
Dennis Rieks <dennisrieks@googlemail.com>
2012-10-03 18:11:15 +0200

    Berkelium: added IPC Constants

--- berkelium/src/include/IpcConstants.hpp
+++ berkelium/src/include/IpcConstants.hpp
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Berkelium-Java constants are located in interface org.berkelium.java.impl.IpcConstants
+
 #ifndef IPCCONSTANTS_HPP_
 #define IPCCONSTANTS_HPP_
 #pragma once
@@ -13,6 +15,9 @@ namespace IpcConstants {
 namespace Delegate {
 
 enum {
+	onPaint = 0, //
+	onTitleChanged = 1, //
+	/*
 	onPaintDone = 0, //
 	onAddressBarChanged = 1, //
 	onStartLoading = 2, //
@@ -25,16 +30,15 @@ enum {
 	freeLastScriptAlert = 9, //
 	onNavigationRequested = 10, //
 	onLoadingStateChanged = 11, //
-	onTitleChanged = 12, //
 	onTooltipChanged = 13, //
 	onCrashed = 14, //
 	onUnresponsive = 15, //
 	onResponsive = 16, //
 	onExternalHost = 17, //
 	onCreatedWindow = 18, //
-	onPaint = 19, //
 	onJavascriptCallback = 20, //
 	onRunFileChooser = 21, //
+	*/
 };
 
 } // namespace Delegate
