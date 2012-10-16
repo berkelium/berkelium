823a361fbbd62fd4e136a84d0a162cf5686e378a
Dennis Rieks <dennisrieks@googlemail.com>
2012-10-16 23:30:21 +0200

    berkelium-api: update script and eclipse launch settings

---
+++ berkelium-api/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Copyright (c) 2012 The Berkelium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+#tmp=$(mktemp)
+#/usr/bin/xsltproc api/api-all.xslt api/api.xml > $tmp
+#chmod +x $tmp
+#cat $tmp
+#$tmp
+#rm $tmp
+
+/usr/bin/xsltproc api/api-all.xslt api/api.xml | while read line; do
+	echo $line
+	bash -c "$line"
+done
