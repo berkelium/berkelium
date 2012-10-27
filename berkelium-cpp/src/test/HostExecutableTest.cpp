44df9362d2084f0edb3d204fb4d6e4c6cfe4f4f5
Dennis Rieks <dennisrieks@googlemail.com>
2012-10-27 23:16:35 +0200

    berkelium-cpp: comments

--- berkelium-cpp/src/test/HostExecutableTest.cpp
+++ berkelium-cpp/src/test/HostExecutableTest.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2012 The Berkelium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "berkelium/HostExecutableFactory.hpp"
 #include "berkelium/HostExecutable.hpp"
 #include "gtest/gtest.h"
--- berkelium-cpp/src/test/test.h
+++ berkelium-cpp/src/test/test.h
@@ -1,2 +1,12 @@
+// Copyright (c) 2012 The Berkelium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BERKELIUM_TEST_TEST_H_
+#define BERKELIUM_TEST_TEST_H_
+#pragma once
+
 #define ASSERT_NULL(ptr) ASSERT_EQ((void*)NULL, (void*)(ptr.get()))
 #define ASSERT_NOT_NULL(ptr) ASSERT_NE((void*)NULL, (void*)(ptr.get()))
+
+#endif // BERKELIUM_TEST_TEST_H_
