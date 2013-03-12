1b752536814f18fa0ef855712baf36357a9363e6
Dennis Rieks <dennisrieks@googlemail.com>
2013-03-12 21:46:16 +0100

    updated chromium patch

--- chromium/patch.sh
+++ chromium/patch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-FILES="src/chrome/browser/ui/gtk/browser_window_gtk.cc src/chrome/browser/ui/browser.cc"
+FILES="src/chrome/browser/ui/gtk/browser_window_gtk.cc src/chrome/app/chrome_main.cc"
 if [ "$1" = "apply" ]; then
 	patch -p0 < chromium-berkelium.patch
 elif [ "$1" = "revert" ]; then
 	patch -R -p0 < chromium-berkelium.patch
 elif [ "$1" = "diff" ]; then
-	svn diff $FILES
+	LANG=C svn diff $FILES
 elif [ "$1" = "create" ]; then
 	$0 diff > chromium-berkelium.patch
 else
