c1e6887dc9c3ac3d66fcf68020147c8a5d50ab20
Dennis Rieks <dennisrieks@googlemail.com>
2013-05-20 21:02:33 +0200

    berkelium-cpp: removed boost code from FilesystemLinux.cpp refs #3

--- berkelium-cpp/src/lib/Impl/FilesystemLinux.cpp
+++ berkelium-cpp/src/lib/Impl/FilesystemLinux.cpp
@@ -2,78 +2,60 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef LINUX
+#ifdef OS_UNIX
 
 #include <Berkelium/Impl/Filesystem.hpp>
 
-#include <boost/filesystem.hpp>
+#include <limits.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 namespace Berkelium {
 
 namespace impl {
 
-std::string Filesystem::append(const std::string& dir, const std::string& append) {
-	boost::filesystem::path a(dir);
-	boost::filesystem::path b(append);
-	return boost::filesystem::path(a / b).string();
-}
+extern const std::string seperator = "/";
 
 std::string Filesystem::absolute(const std::string& arg) {
-	return boost::filesystem::absolute(boost::filesystem::path(arg)).string();
+	char ret[PATH_MAX];
+	if(realpath(arg.c_str(), ret) == 0) {
+		// TODO
+		return "";
+	}
+	return ret;
 }
 
 void Filesystem::createDirectories(const std::string& dir) {
-	boost::filesystem::create_directories(boost::filesystem::path(dir));
+	mkdir(dir.c_str(), 700);
 }
 
 void Filesystem::createDirectoriesFor(const std::string& file) {
-	createDirectories(boost::filesystem::path(file).parent_path().string());
 }
 
 void Filesystem::removeFile(const std::string& file) {
-	boost::filesystem::remove(boost::filesystem::path(file));
 }
 
 void Filesystem::removeDir(const std::string& dir) {
-	boost::filesystem::remove_all(boost::filesystem::path(dir));
 }
 
 bool Filesystem::exists(const std::string& arg) {
-	return boost::filesystem::exists(boost::filesystem::path(arg));
+	return false;
 }
 
 bool Filesystem::readDirectory(const std::string& dir, std::vector<std::string>& content) {
-	boost::filesystem::path tmp(dir);
-	if(!boost::filesystem::is_directory(dir)) {
-		return false;
-	}
-	for(boost::filesystem::directory_iterator itr(tmp); itr != boost::filesystem::directory_iterator(); ++itr) {
-		content.push_back(itr->path().string());
-	}
-	return true;
+	return false;
 }
 
 bool Filesystem::readSymlink(const std::string& symlink, std::string& target) {
-	boost::system::error_code ec;
-	boost::filesystem::path read(boost::filesystem::read_symlink(boost::filesystem::path(symlink), ec));
-	if(ec) {
-		return false;
-	}
-	target = read.string();
-	return true;
+	return false;
 }
 
 bool Filesystem::createSymlink(const std::string& symlink, const std::string& target) {
-	boost::system::error_code ec;
-	boost::filesystem::create_symlink(boost::filesystem::path(target), boost::filesystem::path(symlink), ec);
-	if(ec) {
-		return false;
-	}
-	return true;
+	return false;
 }
 
 } // namespace impl
 
 } // namespace Berkelium
 
-#endif // LINUX
+#endif // OS_UNIX
