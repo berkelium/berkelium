ca87e202ec4b9d6c08c7892a12ad95b5e6748f8a
Dennis Rieks <dennisrieks@googlemail.com>
2013-07-31 01:08:06 +0200

    berkelium-java: JNI Support in CMakeLists.txt

---
+++ berkelium-java/src/main/cpp/Runtime.cpp
@@ -0,0 +1,9 @@
+#include <Berkelium/API/Runtime.hpp>
+
+#include "org_berkelium_impl_RuntimeImpl.h"
+
+
+JNIEXPORT jobject JNICALL Java_org_berkelium_impl_RuntimeImpl_forSystemInstalled(JNIEnv *, jobject)
+{
+	return NULL;
+}
