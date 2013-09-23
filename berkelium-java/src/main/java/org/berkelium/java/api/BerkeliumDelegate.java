ad036c055d5ea1d054cd995f7f77469c5efe74db
Dennis Rieks <dennisrieks@googlemail.com>
2013-09-23 21:31:27 +0200

    berkelium-java: removed old code

--- berkelium-java/src/main/java/org/berkelium/java/api/BerkeliumDelegate.java
+++
@@ -1,7 +0,0 @@
-package org.berkelium.java.api;
-
-public interface BerkeliumDelegate {
-	void onCrashed(Berkelium runtime);
-
-	void onClosed(Berkelium runtime);
-}
--- berkelium-java/src/main/java/org/berkelium/java/api/LogHandler.java
+++
@@ -1,5 +0,0 @@
-package org.berkelium.java.api;
-
-public interface LogHandler {
-	void log(String message);
-}
